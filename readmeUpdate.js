6ae46ee614f1398aa4f044b1263a13e1e65546b7
이수창 <suchasucha@naver.com>
2023-03-30 15:13:01 +0900

    Update: ReadMe 변경

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -7,16 +7,31 @@ import Parser from "rss-parser";
  */
 let text = `# Hi there 👋
 
-## 이런 환경에 익숙해요✍🏼
+<div align = 'center'>
+  
+![header](https://capsule-render.vercel.app/api?type=waving&height=250&text=SuchangLee&fontSize=90)
+ <img src="http://mazassumnida.wtf/api/v2/generate_badge?boj=eternalclash97">
+  <p>Welcome</p>
 
-## 언어
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/>
+<img src="https://img.shields.io/badge/React-61DAFB?style=flat-square&logo=React&logoColor=white"/>
+<img src="https://img.shields.io/badge/Redux-764ABC?style=flat-square&logo=Redux&logoColor=white"/>
+  <img src="https://img.shields.io/badge/Java-00A98F?style=flat-square&logo=Java&logoColor=white"/> 
+  <img src="https://img.shields.io/badge/Node.js-339933?style=flat-square&logo=Node.js&logoColor=white"/>
 
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/> 
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
-
-## Contact me
+  <p>My stats</p>
+  
+  
+<a href="https://github.com/anuraghazra/github-readme-stats">
+  <img src="https://github-readme-stats.vercel.app/api?username=eternalclash&show_icons=true&theme=material-palenight&hide_border=true&bg_color=20232a&icon_color=E3E3E3A8&text_color=fff" width=49.2% />
+</a>
+<a href="https://github.com/denvercoder1/github-readme-streak-stats">
+  <img src="http://github-readme-streak-stats.herokuapp.com?user=eternalclash&theme=react&ring=C691E94D&fire=C691E9&sideNums=C691E9&currStreakNum=C691E9&sideLabels=FFFFFF&currStreakLabel=FFFFFF&dates=E3E3E3A8&hide_border=true" width=49.2% />
+</a>
+<a href="https://github.com/ashutosh00710/github-readme-activity-graph">
+<img src="https://activity-graph.herokuapp.com/graph?username=eternalclash&theme=react-dark&bg_color=20232a&hide_border=true&line=AB90E8&color=C691E9C9" width=98%/>
+</a>
+  </div>
 
 ## 📕 Latest Blog Posts
 
